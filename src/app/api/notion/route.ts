fbb84a545397e5bf01f3351934a74175071683e9
M1n01 <abe.minato.bz@gmail.com>
2024-11-23 18:42:58 +0900

    fix: article property name

--- src/app/api/notion/route.ts
+++ src/app/api/notion/route.ts
@@ -19,7 +19,7 @@ export async function GET() {
       database_id: process.env.NEXT_PUBLIC_DATABASE_ID!,
       sorts: [
         {
-          property: "PublishedAt",
+          property: "publishedAt",
           direction: "descending",
         },
       ],
