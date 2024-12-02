6d3a045669dd759ccbb4d105b2567727cba94151
M1n01 <abe.minato.bz@gmail.com>
2024-12-02 16:35:29 +0900

    #1 update: remove any in api/nosion

--- src/app/api/notion/route.ts
+++ src/app/api/notion/route.ts
@@ -2,6 +2,14 @@ import { corsHeaders } from "../cors";
 import { NextResponse } from "next/server";
 import { getAllArticles } from "../../../lib/notion";
 import { Article } from "../../../types/notion/Article";
+import { type Tag } from "../../../types/notion/Tag";
+import { isFullPageOrDatabase } from "@notionhq/client";
+import {
+  type PageObjectResponse,
+  PartialPageObjectResponse,
+  PartialDatabaseObjectResponse,
+  DatabaseObjectResponse,
+} from "@notionhq/client/build/src/api-endpoints";
 
 export async function OPTIONS() {
   return NextResponse.json({}, { headers: corsHeaders });
@@ -9,23 +17,56 @@ export async function OPTIONS() {
 
 export async function GET() {
   try {
-    const response = await getAllArticles();
+    const response = (await getAllArticles()).results;
     if (response) {
-      const articles: Article[] = response.results.map((post: any) => {
-        return {
-          id: post.id,
-          thumbnail: post.cover?.external.url,
-          title: post.properties.title.title[0].plain_text,
-          description: post.properties.description.rich_text[0]?.plain_text,
-          publishedAt: post.properties.publishedAt.date.start,
-          updatedAt: post.last_edited_time,
-          tags: post.properties.tags.multi_select.map((tag: any) => ({
-            id: tag.id,
-            name: tag.name,
-            color: tag.color,
-          })),
-          content: undefined,
-        };
+      const articles: Article[] = response.map((post) => {
+        if (isFullPageOrDatabase(post)) {
+          return {
+            id: post.id,
+            thumbnail:
+              post.cover?.type === "external"
+                ? post.cover.external.url
+                : undefined,
+            title:
+              post.properties.title.type === "title"
+                ? post.properties.title.title
+                    .map((title) => title.plain_text)
+                    .join("")
+                : "",
+            description:
+              post.properties.description.type === "rich_text"
+                ? post.properties.description.rich_text
+                    .map((description) => description.plain_text)
+                    .join("")
+                : "",
+            publishedAt:
+              post.properties.publishedAt.type === "date"
+                ? (post.properties.publishedAt.date?.start ?? "")
+                : "",
+            updatedAt: post.last_edited_time,
+            tags:
+              post.properties.tags.type === "multi_select" &&
+              Array.isArray(post.properties.tags.multi_select)
+                ? post.properties.tags.multi_select.map((tag: Tag) => ({
+                    id: tag.id,
+                    name: tag.name,
+                    color: tag.color,
+                  }))
+                : [],
+            content: undefined,
+          };
+        } else {
+          return {
+            id: post.id,
+            thumbnail: undefined,
+            title: "",
+            description: "",
+            publishedAt: "",
+            updatedAt: "",
+            tags: [],
+            content: undefined,
+          };
+        }
       });
       return new NextResponse(JSON.stringify(articles), {
         status: 200,
