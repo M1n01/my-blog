3aee60e5925be632823b9e1e574a93089c035cff
M1n01 <abe.minato.bz@gmail.com>
2024-12-02 11:29:35 +0900

    #4 fix: skelton style

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -152,7 +152,7 @@ const BlogContents: FC = () => {
     <Layout>
       {loading ? (
         <Container size="md" py="xl">
-          <Skeleton height={450} />
+          <Skeleton height={450} mb="xl" />
           <Skeleton height={50} mb="xl" />
           <Skeleton height={30} mb="md" />
           <Skeleton height={300} mb="md" />
