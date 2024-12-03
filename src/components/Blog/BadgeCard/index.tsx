1604dbe4163504404523e8c27a8189dd9dc8eb42
M1n01 <abe.minato.bz@gmail.com>
2024-12-03 11:09:53 +0900

    fix: add 'use clients' in components

--- src/components/Blog/BadgeCard/index.tsx
+++ src/components/Blog/BadgeCard/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 import { Badge, Card, Image, Text, Group } from "@mantine/core";
 import { Article } from "../../../types/notion/Article";
--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { FC } from "react";
 import { AppShell } from "@mantine/core";
 
