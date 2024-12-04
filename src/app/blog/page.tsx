98a897ed881be10e588998cfff640656ae30bba2
M1n01 <abe.minato.bz@gmail.com>
2024-12-04 16:59:06 +0900

    fix: suspend components

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -50,14 +50,15 @@ export default async function BlogList() {
     <Layout>
       <Container size="lg" py="xl">
         <Title order={1}>Blog</Title>
-        <Suspense fallback={<LoadingGrid />} />
-        <Grid>
-          {posts.map((post: Article) => (
-            <GridCol key={post.id} span={{ base: 12, sm: 6, md: 4 }}>
-              <BadgeCard post={post} />
-            </GridCol>
-          ))}
-        </Grid>
+        <Suspense fallback={<LoadingGrid />}>
+          <Grid>
+            {posts.map((post: Article) => (
+              <GridCol key={post.id} span={{ base: 12, sm: 6, md: 4 }}>
+                <BadgeCard post={post} />
+              </GridCol>
+            ))}
+          </Grid>
+        </Suspense>
 
         {/* {!loading && Math.ceil(posts.length / postsPerPage) > 1 && (
           <Group justify="center" mt="xl">
