ff7928d7be2edf08ef19e111582d5f2849da797d
M1n01 <abe.minato.bz@gmail.com>
2024-12-05 19:32:14 +0900

    refactor: delete unuse file'

--- src/app/api/cors.ts
+++
@@ -1,5 +0,0 @@
-export const corsHeaders = {
-  "Access-Control-Allow-Origin": "http://localhost:3000",
-  "Access-Control-Allow-Methods": "GET, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
-};
