1bebe94f9a321361a321232c214c5b2a25ad50c9
M1n01 <abe.minato.bz@gmail.com>
2024-12-05 10:04:11 +0900

    test

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,8 @@
-import { Suspense } from "react";
 import { headers } from "next/headers";
 import { Container, Title, Grid, GridCol } from "@mantine/core";
 import BadgeCard from "./BadgeCard";
 
 import Layout from "./_layout";
-import LoadingGrid from "./loading";
 import { Article } from "@/types/notion/Article";
 
 export const runtime = "edge";
@@ -50,15 +48,13 @@ export default async function BlogList() {
     <Layout>
       <Container size="lg" py="xl">
         <Title order={1}>Blog</Title>
-        <Suspense fallback={<LoadingGrid />}>
-          <Grid>
-            {posts.map((post: Article) => (
-              <GridCol key={post.id} span={{ base: 12, sm: 6, md: 4 }}>
-                <BadgeCard post={post} />
-              </GridCol>
-            ))}
-          </Grid>
-        </Suspense>
+        <Grid>
+          {posts.map((post: Article) => (
+            <GridCol key={post.id} span={{ base: 12, sm: 6, md: 4 }}>
+              <BadgeCard post={post} />
+            </GridCol>
+          ))}
+        </Grid>
 
         {/* {!loading && Math.ceil(posts.length / postsPerPage) > 1 && (
           <Group justify="center" mt="xl">
