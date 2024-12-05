b28b0044aa03897f253aa58ff5ce5403d2041392
M1n01 <abe.minato.bz@gmail.com>
2024-12-05 11:04:38 +0900

    fix: the way to retrieve articles

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,44 +1,17 @@
-import { headers } from "next/headers";
 import { Container, Title, Grid, GridCol } from "@mantine/core";
 import BadgeCard from "./BadgeCard";
+import { getAllArticles } from "@/lib/notion";
 
 import Layout from "./_layout";
 import { Article } from "@/types/notion/Article";
 
 export const runtime = "edge";
 
-async function getArticles() {
-  const headersData = await headers();
-  const host = headersData.get("host");
-  const protocol = headersData.get("x-forwarded-proto") || "http";
-  const origin = `${protocol}://${host}`;
-  const url = `${origin}/api/notion`;
-  console.log("url:", url);
-
-  try {
-    const res = await fetch(url, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (!res.ok) {
-      throw new Error("Failed to fetch articles");
-    }
-
-    return res.json();
-  } catch (error) {
-    console.error("Failed to fetch articles:", error);
-    return [];
-  }
-}
-
 export default async function BlogList() {
   // const [activePage, setActivePage] = useState(1);
   // const postsPerPage = 9;
 
-  const posts = await getArticles();
+  const posts = await getAllArticles();
   console.log("posts:\n", posts);
 
   // const paginatedPosts = posts.slice(
