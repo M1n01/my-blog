df922955490ec14a7285a8966a745859a9803489
M1n01 <abe.minato.bz@gmail.com>
2024-12-07 14:54:30 +0900

    update: loading style

--- src/app/blog/loading.tsx
+++ src/app/blog/loading.tsx
@@ -1,16 +1,22 @@
 "use client";
 import { Grid, GridCol, Skeleton } from "@mantine/core";
+import Layout from "./_layout";
 
 export default function LoadingGrid() {
   return (
-    <Grid>
-      {Array(3)
-        .fill(0)
-        .map((_, index) => (
-          <GridCol key={`skeleton-${index}`} span={{ base: 12, sm: 6, md: 4 }}>
-            <Skeleton height={200} radius="md" mb="xl" />
-          </GridCol>
-        ))}
-    </Grid>
+    <Layout>
+      <Grid>
+        {Array(3)
+          .fill(0)
+          .map((_, index) => (
+            <GridCol
+              key={`skeleton-${index}`}
+              span={{ base: 12, sm: 6, md: 4 }}
+            >
+              <Skeleton height={200} radius="md" mb="xl" />
+            </GridCol>
+          ))}
+      </Grid>
+    </Layout>
   );
 }
