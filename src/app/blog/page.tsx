50620d01c45215f18d0444e2133219cda6f7467c
M1n01 <abe.minato.bz@gmail.com>
2024-12-17 16:53:41 +0900

    aaa

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,9 +2,7 @@
 // import { Alert, Container, Title, Grid, GridCol } from "@mantine/core";
 // import BadgeCard from "./BadgeCard";
 // import { getAllArticles } from "@/lib/notion";
-import { Container } from "@mantine/core";
 
-import LoadingGrid from "./loading";
 // import { Article } from "@/types/notion/Article";
 
 export const runtime = "edge";
@@ -31,38 +29,31 @@ export default async function BlogList() {
   //   activePage * postsPerPage,
   // );
   return (
-    <Container size="lg" py="xl">
-      {/* {error ? (
-        <Alert color="red" title="Error">
-          {error}
-        </Alert>
-      ) : (
-        <>
-          <Title order={2} mb="lg">
-            Articles
-          </Title>
-          <Suspense fallback={<LoadingGrid />}>
-            <Grid gutter="lg">
-              {posts?.map((post: Article) => (
-                <GridCol span={{ base: 12, sm: 6, md: 4 }} key={post.id}>
-                  <BadgeCard post={post} />
-                </GridCol>
-              ))}
-            </Grid>
-          </Suspense>
-        </>
-      )} */}
-      <LoadingGrid />
+    <>
+      <h1>BlogList</h1>
+    </>
+    // <Container size="lg" py="xl">
+    //   {error ? (
+    //     <Alert color="red" title="Error">
+    //       {error}
+    //     </Alert>
+    //   ) : (
+    //     <>
+    //       <Title order={2} mb="lg">
+    //         Articles
+    //       </Title>
+    //       <Suspense fallback={<LoadingGrid />}>
+    //         <Grid gutter="lg">
+    //           {posts?.map((post: Article) => (
+    //             <GridCol span={{ base: 12, sm: 6, md: 4 }} key={post.id}>
+    //               <BadgeCard post={post} />
+    //             </GridCol>
+    //           ))}
+    //         </Grid>
+    //       </Suspense>
+    //     </>
+    //   )}
 
-      {/* {!loading && Math.ceil(posts.length / postsPerPage) > 1 && (
-          <Group justify="center" mt="xl">
-            <Pagination
-              value={activePage}
-              onChange={setActivePage}
-              total={Math.ceil(posts.length / postsPerPage)}
-            />
-          </Group>
-        )} */}
-    </Container>
+    // </Container>
   );
 }
