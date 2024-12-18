0b1654559d9a5fd7e924b628a7c23b1e5835bf29
M1n01 <abe.minato.bz@gmail.com>
2024-12-18 17:31:49 +0900

    fix: test

---
+++ src/app/test/page.tsx
@@ -0,0 +1,12 @@
+import { Text } from "@mantine/core";
+
+export default function Page() {
+  return (
+    <>
+      <Text size="sm">
+        I am an engineer from 42 Tokyo. I&apos;m interested in Rust and
+        TypeScript, and I&apos;m also passionate about Web3.0 technologies
+      </Text>
+    </>
+  );
+}
