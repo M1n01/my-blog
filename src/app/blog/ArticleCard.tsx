4d07902b3a6010a728ea95c8c20bf0a18bf541ed
M1n01 <abe.minato.bz@gmail.com>
2024-12-20 15:16:57 +0900

    test3

--- src/app/blog/ArticleCard.tsx
+++ src/app/blog/ArticleCard.tsx
@@ -1,53 +1,25 @@
 "use client";
 
 import { FC } from "react";
-import { Badge, Card, CardSection, Image, Text, Group } from "@mantine/core";
-import { type Article } from "../../types/notion/Article";
 import classes from "./ArticleCard.module.css";
+import { type Article } from "../../types/notion/Article";
 
 const ArticleCard: FC<{ post: Article }> = ({ post }) => {
-  const { id, thumbnail, title, description, publishedAt, tags } = post;
+  const { id, title, description, publishedAt, tags } = post;
   console.log("post:", post);
   const labels = tags?.map((tag) => (
-    <Badge key={`${tag.id}`} color={`${tag.color!}`} variant="light">
+    <span key={`${tag.id}`} style={{ backgroundColor: tag.color }}>
       {tag.name}
-    </Badge>
+    </span>
   ));
 
   return (
-    <Card
-      withBorder
-      shadow="sm"
-      radius="md"
-      p="md"
-      className={classes.card}
-      component="a"
-      href={`/blog/${id}`}
-    >
-      <CardSection>
-        <Image src={thumbnail} alt={title} height={180} />
-      </CardSection>
-      <CardSection className={classes.section} mt="md" style={{ height: 200 }}>
-        <Group justify="space-between">
-          <Text fz="lg" fw={500}>
-            {title}
-          </Text>
-          <Badge size="sm" variant="light">
-            {publishedAt}
-          </Badge>
-        </Group>
-        <Text fz="sm" mt="xs">
-          {description}
-        </Text>
-      </CardSection>
-
-      <CardSection className={classes.section}>
-        <Text mt="md" mb="md" className={classes.label} c="dimmed">
-          Tags
-        </Text>
-        <Group gap={7}>{labels}</Group>
-      </CardSection>
-    </Card>
+    <div className={classes.card}>
+      <div>{title}</div>
+      <div>{publishedAt}</div>
+      <div>{description}</div>
+      <div>Tags: {labels}</div>
+    </div>
   );
 };
 
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,6 @@
-import { Suspense } from "react";
-import { Alert, Container, Title, Grid, GridCol } from "@mantine/core";
+import { Container, GridCol } from "@mantine/core";
 import ArticleCard from "./ArticleCard";
 import { getAllArticles } from "@/lib/notion";
-
-import LoadingGrid from "./loading";
 import { Article } from "@/types/notion/Article";
 
 export const runtime = "edge";
@@ -13,17 +10,7 @@ export default async function BlogList() {
   // const [activePage, setActivePage] = useState(1);
   // const postsPerPage = 9;
 
-  let posts;
-  let error;
-
-  try {
-    posts = await getAllArticles();
-  } catch (e) {
-    error =
-      e instanceof Error
-        ? e.message
-        : "An error occurred while fetching articles.";
-  }
+  const posts = await getAllArticles();
 
   // const paginatedPosts = posts.slice(
   //   (activePage - 1) * postsPerPage,
@@ -31,26 +18,14 @@ export default async function BlogList() {
   // );
   return (
     <Container size="lg" py="xl">
-      {error ? (
-        <Alert color="red" title="Error">
-          {error}
-        </Alert>
-      ) : (
-        <>
-          <Title order={2} mb="lg">
-            Articles
-          </Title>
-          <Suspense fallback={<LoadingGrid />}>
-            <Grid gutter="lg">
-              {posts?.map((post: Article) => (
-                <GridCol span={{ base: 12, sm: 6, md: 4 }} key={post.id}>
-                  <ArticleCard post={post} />
-                </GridCol>
-              ))}
-            </Grid>
-          </Suspense>
-        </>
-      )}
+      {posts?.map((post: Article) => {
+        console.log("Passing post to ArticleCard:", post); // 👈  追加
+        return (
+          <GridCol span={{ base: 12, sm: 6, md: 4 }} key={post.id}>
+            <ArticleCard post={post} />
+          </GridCol>
+        );
+      })}
 
       {/* {!loading && Math.ceil(posts.length / postsPerPage) > 1 && (
           <Group justify="center" mt="xl">
