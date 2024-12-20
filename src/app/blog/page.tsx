5d1c7af52ac5e3d23a066799ef8b6870ad8e9347
M1n01 <abe.minato.bz@gmail.com>
2024-12-20 14:21:50 +0900

    test

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,13 +1,13 @@
 "use client";
-import { Text } from "@mantine/core";
+import { Text, Container } from "@mantine/core";
 
 export default function Page() {
   return (
-    <>
+    <Container>
       <Text size="sm">
         I am an engineer from 42 Tokyo. I&apos;m interested in Rust and
         TypeScript, and I&apos;m also passionate about Web3.0 technologies
       </Text>
-    </>
+    </Container>
   );
 }
