1406696928115574958636da3238026140080f72
M1n01 <abe.minato.bz@gmail.com>
2024-12-23 11:54:07 +0900

    #11 feat: add loading on likes button

--- src/app/blog/[id]/Likes.tsx
+++ src/app/blog/[id]/Likes.tsx
@@ -1,17 +1,33 @@
+"use client";
+
 import { Text, ActionIcon, Group } from "@mantine/core";
 import { IconHeart } from "@tabler/icons-react";
+import useSWR from "swr";
+import { useParams } from "next/navigation";
 
-// export const Likes = ({ likes }: { likes: number }) => {
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+function useLikes() {
+  const params = useParams();
+  const { data, error } = useSWR(`/api/likes/${params.id}`, fetcher);
+
+  return {
+    likes: data,
+    isLoading: !error && !data,
+    isError: error,
+  };
+}
+
+// likesボタンを押したら、SWRを使っていいね数を取得する
 export const Likes = () => {
+  const { likes, isLoading } = useLikes();
+
   return (
     <Group align="center">
-      <ActionIcon variant="outline" c="red" radius="xl">
+      <ActionIcon variant="outline" c="red" radius="xl" loading={isLoading}>
         <IconHeart size={18} stroke={1.5} />
       </ActionIcon>
-      <Text>
-        {/* {likes} likes */}
-        15
-      </Text>
+      {isLoading ? <Text>Loading...</Text> : <Text>{likes}</Text>}
     </Group>
   );
 };
