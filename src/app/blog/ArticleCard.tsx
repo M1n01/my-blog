ed128a824a7936d3ba14b59885882062704722ba
M1n01 <abe.minato.bz@gmail.com>
2025-01-16 11:30:54 +0900

    #23 fix: ArticleCard

--- src/app/blog/ArticleCard.tsx
+++ src/app/blog/ArticleCard.tsx
@@ -1,14 +1,24 @@
 import { FC } from "react";
-import { Badge, Card, CardSection, Image, Text, Group } from "@mantine/core";
+import {
+  Badge,
+  Card,
+  CardSection,
+  Image,
+  Text,
+  Title,
+  Group,
+  Stack,
+} from "@mantine/core";
 import { type Article } from "../../types/notion/Article";
 import classes from "./ArticleCard.module.css";
+import { IconCalendarTime } from "@tabler/icons-react";
 
 const ArticleCard: FC<{ post: Article }> = ({ post }) => {
   const { id, thumbnail, title, description, publishedAt, category, tags } =
     post;
   console.log("post:", post);
   const labels = tags?.map((tag) => (
-    <Badge key={`${tag.id}`} color={`${tag.color!}`} variant="light">
+    <Badge key={`${tag.id}`} color={`${tag.color!}`} variant="dot">
       {tag.name}
     </Badge>
   ));
@@ -26,25 +36,32 @@ const ArticleCard: FC<{ post: Article }> = ({ post }) => {
       <CardSection>
         <Image src={thumbnail} alt={title} height={180} />
       </CardSection>
-      <CardSection className={classes.section} mt="md" style={{ height: 200 }}>
+      <CardSection className={classes.section} style={{ height: 250 }}>
         <Group justify="space-between" mb="sm">
-          <Badge size="sm" variant="light">
-            {publishedAt}
+          <Badge size="md" variant="light">
+            <IconCalendarTime size={12} /> {publishedAt}
           </Badge>
-          <Badge size="sm" variant="light" color={category.color || "blue"}>
+          <Badge
+            size="md"
+            variant="transparent"
+            color={category.color || "blue"}
+          >
             {category.name}
           </Badge>
         </Group>
-        <Text size="sm" fz="lg" fw={500}>
+        <Title order={4} mt="xs" mb="sm">
           {title}
-        </Text>
-        <Text size="xs" fz="sm" mt="xs">
-          {description}
-        </Text>
+        </Title>
+        <Stack mt="sm" gap="xs">
+          <Text size="xs">【内容】</Text>
+          <Text size="xs" fz="sm">
+            {description}
+          </Text>
+        </Stack>
       </CardSection>
 
       <CardSection className={classes.section} mih={90}>
-        <Text mt="md" mb="lg" className={classes.label} c="dimmed">
+        <Text mb="lg" className={classes.label} c="dimmed">
           Tags
         </Text>
         <Group gap={7}>{labels}</Group>
