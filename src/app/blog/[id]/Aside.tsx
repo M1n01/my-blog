f4c0edd6442e2270ebe8551aea1d871771319c8e
M1n01 <abe.minato.bz@gmail.com>
2025-01-18 19:37:04 +0900

    #13 feat: aside

---
+++ src/app/blog/[id]/Aside.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { AppShell } from "@mantine/core";
+
+export default function BlogContentAside() {
+  return (
+    <AppShell.Aside>
+      <h1>BlogContentAside</h1>
+    </AppShell.Aside>
+  );
+}
---
+++ src/app/blog/[id]/TableOfContents.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { TableOfContents } from "@mantine/core";
+
+export const TableOfContent = ({ children }: { children: React.ReactNode }) => {
+  return <TableOfContents>{children}</TableOfContents>;
+};
