9dd8e3e3731e80abc8aff77de8bfb67db562d11b
M1n01 <abe.minato.bz@gmail.com>
2025-01-23 12:56:22 +0900

    update: navbar self introduction

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -9,7 +9,12 @@ import {
   Group,
   Button,
 } from "@mantine/core";
-import { IconBrandGithub, IconBrandX, IconHeart } from "@tabler/icons-react";
+import {
+  IconBrandGithub,
+  IconBrandX,
+  IconHeart,
+  IconMapPin,
+} from "@tabler/icons-react";
 
 const Navbar: FC<{ opened: boolean }> = ({ opened }) => {
   return (
@@ -20,15 +25,20 @@ const Navbar: FC<{ opened: boolean }> = ({ opened }) => {
           <Avatar size="lg" src="/assets/avatar.jpg" />
           <Title order={3}>Abe Minato</Title>
           <Text size="sm" c="dimmed">
-            Beginner Developer
+            Developer
+          </Text>
+          <Text size="md" c="">
+            <IconMapPin size={15} /> Tokyo
           </Text>
         </Stack>
 
         {/* Bio Section */}
         <Stack gap="xs">
           <Text size="sm">
-            I am an engineer from 42 Tokyo. I&apos;m interested in Rust and
-            TypeScript, and I&apos;m also passionate about Web3.0 technologies.
+            42Tokyo出身のエンジニア。札幌出身。フロントエンドを主に触っています。
+          </Text>
+          <Text size="sm">
+            趣味は、野球観戦、読書、アニメ・マンガ、映画、美術館、コーヒーなど。好きな動物はテッポウエビ。飼いたい動物はユーラシアワシミミズクとボールパイソン。
           </Text>
         </Stack>
 
