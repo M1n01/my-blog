6611d43eb4b14c85414cc99bc180ab770671bf95
M1n01 <abe.minato.bz@gmail.com>
2025-02-25 18:20:50 +0900

    #25 udpate: clicking Image, open modal

--- src/components/blocks/ImageBlock.tsx
+++ src/components/blocks/ImageBlock.tsx
@@ -1,5 +1,7 @@
-import React from "react";
-import { Image } from "@mantine/core";
+"use client";
+
+import React, { useState } from "react";
+import { Image, Modal, Box } from "@mantine/core";
 import { ImageBlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 
 export const ImageBlock = ({
@@ -9,21 +11,51 @@ export const ImageBlock = ({
   block: ImageBlockObjectResponse;
   index: number;
 }) => {
+  const [opened, setOpened] = useState(false);
+  const imageUrl =
+    block.image.type === "external"
+      ? block.image.external.url
+      : block.image.file.url;
+  const imageAlt = block.image.caption.map((text) => text.plain_text).join(" ");
+
   return (
-    <Image
-      key={index}
-      src={
-        block.image.type === "external"
-          ? block.image.external.url
-          : block.image.file.url
-      }
-      alt={block.image.caption.map((text) => text.plain_text).join(" ")}
-      w="auto"
-      fit="contain"
-      radius="md"
-      mah={300}
-      mb="lg"
-      mt="lg"
-    />
+    <>
+      <Image
+        key={index}
+        src={imageUrl}
+        alt={imageAlt}
+        w="auto"
+        fit="contain"
+        radius="md"
+        mah={300}
+        mb="lg"
+        mt="lg"
+        style={{ cursor: "pointer" }}
+        onClick={() => setOpened(true)}
+      />
+
+      <Modal
+        opened={opened}
+        onClose={() => setOpened(false)}
+        size="xl"
+        padding="md"
+        centered
+      >
+        <Box
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            src={imageUrl}
+            alt={imageAlt}
+            fit="contain"
+            style={{ maxHeight: "90vh" }}
+          />
+        </Box>
+      </Modal>
+    </>
   );
 };
