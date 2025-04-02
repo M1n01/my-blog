dbdccb7ab14802ae8f27aff692683b969d913265
M1n01 <abe.minato.bz@gmail.com>
2025-04-03 08:08:15 +0900

    #37 refactor: replace href with Link component for ArticleCard navigation

--- src/components/blog/ArticleCard.tsx
+++ src/components/blog/ArticleCard.tsx
@@ -12,6 +12,7 @@ import {
 import { type Article } from "@/types/notion/Article";
 import classes from "./ArticleCard.module.css";
 import { IconCalendarTime, IconCategory, IconTags } from "@tabler/icons-react";
+import Link from "next/link";
 
 const ArticleCard: FC<{ post: Article }> = ({ post }) => {
   const { id, thumbnail, title, description, publishedAt, category, tags } =
@@ -24,58 +25,59 @@ const ArticleCard: FC<{ post: Article }> = ({ post }) => {
   ));
 
   return (
-    <Card
-      withBorder
-      shadow="sm"
-      radius="md"
-      p="md"
-      className={classes.card}
-      component="a"
-      href={`/blog/${id}`}
-    >
-      <CardSection>
-        <Image src={thumbnail} alt={title} height={180} />
-      </CardSection>
-      <CardSection className={classes.section} style={{ height: 250 }}>
-        <Group justify="space-between" mb="sm">
-          <Badge size="md" variant="light">
-            <IconCalendarTime size={12} /> {publishedAt}
-          </Badge>
-          <Badge
-            size="md"
-            variant="transparent"
-            color={category.color || "blue"}
+    <Link href={`/blog/${id}`} passHref legacyBehavior>
+      <Card
+        withBorder
+        shadow="sm"
+        radius="md"
+        p="md"
+        className={classes.card}
+        component="a"
+      >
+        <CardSection>
+          <Image src={thumbnail} alt={title} height={180} />
+        </CardSection>
+        <CardSection className={classes.section} style={{ height: 250 }}>
+          <Group justify="space-between" mb="sm">
+            <Badge size="md" variant="light">
+              <IconCalendarTime size={12} /> {publishedAt}
+            </Badge>
+            <Badge
+              size="md"
+              variant="transparent"
+              color={category.color || "blue"}
+            >
+              <IconCategory size={12} /> {category.name}
+            </Badge>
+          </Group>
+          <Title order={4} mt="xs" mb="sm">
+            {title}
+          </Title>
+          <Stack
+            mt="sm"
+            gap="xs"
+            style={{ overflow: "hidden", maxHeight: "120px" }}
           >
-            <IconCategory size={12} /> {category.name}
-          </Badge>
-        </Group>
-        <Title order={4} mt="xs" mb="sm">
-          {title}
-        </Title>
-        <Stack
-          mt="sm"
-          gap="xs"
-          style={{ overflow: "hidden", maxHeight: "120px" }}
-        >
-          <Text size="xs">【内容】</Text>
-          <Text
-            size="xs"
-            fz="sm"
-            lineClamp={4}
-            style={{ overflow: "hidden", textOverflow: "ellipsis" }}
-          >
-            {description}
-          </Text>
-        </Stack>
-      </CardSection>
+            <Text size="xs">【内容】</Text>
+            <Text
+              size="xs"
+              fz="sm"
+              lineClamp={4}
+              style={{ overflow: "hidden", textOverflow: "ellipsis" }}
+            >
+              {description}
+            </Text>
+          </Stack>
+        </CardSection>
 
-      <CardSection className={classes.section} mih={90}>
-        <Text mb="lg" className={classes.label} c="dimmed">
-          <IconTags size={15} /> Tags
-        </Text>
-        <Group gap={7}>{labels}</Group>
-      </CardSection>
-    </Card>
+        <CardSection className={classes.section} mih={90}>
+          <Text mb="lg" className={classes.label} c="dimmed">
+            <IconTags size={15} /> Tags
+          </Text>
+          <Group gap={7}>{labels}</Group>
+        </CardSection>
+      </Card>
+    </Link>
   );
 };
 
